7a849dce075388517ae3dd2e60308739ee6de3ca
goodhzy <qq.com>
2020-06-03 17:44:23 +0800

    4-6 初始化博客和用户登录路由

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,17 +1,27 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = ((req, res)=>{
     // 设置返回格式
     res.setHeader('Content-type', 'application/json')
-    try {
-        const resData = {
-        name: 'hzy',
-        site: 'imooc',
-        evn: process.env.NODE_ENV
-    }
-    res.end(JSON.stringify(resData))
-    } catch (error) {
-        console.log('报错了');
+    
+    const blogData = handleBlogRouter(req,res)
+    if(blogData){
+        res.end(JSON.stringify(blogData))
+        return
     }
     
+    const userData = handleUserRouter(req,res)
+    if(userData){
+        res.end(JSON.stringify(userData))
+        return
+    }
+
+    // 未命中路由,返回404
+    res.writeHead(404,{'Content-type': 'text/plain'})
+    res.write('404 NOT FOUND\n')
+    res.end()
+
 })
 
 module.exports = serverHandle
